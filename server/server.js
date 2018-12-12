0ea983832c36980935d305db5894326860ac257d
Kiet <bunbothomngon@gmail.com>
2018-12-12 10:18:03 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -29,6 +30,28 @@ app.get('/todos', (req, res) => {
   }, err => {
     res.status(400).send(err);
   });
+});
+
+// GET TODO BY ID
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  // Validate id
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then(todo => {
+      // The call is succeed, but ID is not found in the collection
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({todo});
+    })
+    .catch( err => {
+      res.status(400).send();
+    });
 });
 
 app.listen(9999, () => {
