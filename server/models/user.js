eee019fd2154a13830c2dd58772f32459a183b58
Kiet <bunbothomngon@gmail.com>
2018-12-14 17:52:45 +0200

    Add DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -57,6 +57,18 @@ UserSchema.methods.generateAuthToken = function() {
   });
 };
 
+UserSchema.methods.removeToken = function(token) {
+  const user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {
+        token: token
+      }
+    }
+  });
+};
+
 /**
   * MODEL METHODS
 **/
